eb2083ed5cc724efa0d41d81b4b3709e6e94c7aa
Jathin-24 <23jr1a05a4@gmail.com>
2026-02-07 21:14:42 +0530

    working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
 
   return (
     <nav className="navbar">
       <h3>College Events</h3>
 
       <div>
-        <Link to="/dashboard">Dashboard</Link>
         <Link to="/events">Events</Link>
-        <Link to="/myevents">My Events</Link>
+        <Link to="/events">Dashboard</Link>
+        <Link to="/my-events">My Events</Link>
         <Link to="/calendar">Calendar</Link>
         <Link to="/map">Map</Link>
-        <button onClick={() => navigate("/login")}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </nav>
   );
